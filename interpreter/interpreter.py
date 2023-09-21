c5924faef5a5ee8d3748b89f1116043b8dbf1cf3
Yoshioka <sss.yoshioka@gmail.com>
2023-09-21 21:54:53 +0000

    feat: 🎸 不要ロジック削除

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -15,8 +15,6 @@ import urllib.parse
 import tokentrim as tt
 from pprint import pprint
 from rich import print
-from rich.markdown import Markdown
-from rich.rule import Rule
 
 function_schema = {
   "name": "run_code",
@@ -44,7 +42,6 @@ class Interpreter:
   def __init__(self):
     self.messages = []
     self.temperature = 0.001
-    self.auto_run = False
     self.local = False
     self.model = "gpt-4"
     self.debug_mode = False
@@ -113,7 +110,6 @@ class Interpreter:
     pprint(messages)
     pprint("====== prompt end ========")
 
-    # Initialize message, function call trackers, and active block
     self.messages.append({})
     in_function_call = False
     self.active_block = None
@@ -125,8 +121,6 @@ class Interpreter:
       condition = "function_call" in self.messages[-1]
       if condition:
         if in_function_call == False:
-
-          # If so, end the last block,
           self.end_active_block()
 
           # Print newline if it was just a code block or user message
@@ -135,17 +129,14 @@ class Interpreter:
           if last_role == "user" or last_role == "function":
             print()
 
-          # then create a new code block
           self.active_block = CodeBlock()
 
         in_function_call = True
 
-        # Parse arguments and save to parsed_arguments, under function_call
         if "arguments" in self.messages[-1]["function_call"]:
           arguments = self.messages[-1]["function_call"]["arguments"]
           new_parsed_arguments = parse_partial_json(arguments)
           if new_parsed_arguments:
-            # Only overwrite what we have if it's not None (which means it failed to parse)
             self.messages[-1]["function_call"][
               "parsed_arguments"] = new_parsed_arguments
 
@@ -166,73 +157,50 @@ class Interpreter:
           print(self.messages[-1])
           print("---")
 
-          # Ask for user confirmation to run code
-          if self.auto_run == False:
-            # End the active block so you can run input() below it
-            # Save language and code so we can create a new block in a moment
+          # End the active block so you can run input() below it
+          # Save language and code so we can create a new block in a moment
+          self.active_block.end()
+          language = self.active_block.language
+          code = self.active_block.code
+
+          response = input("  Would you like to run this code? (y/n)\n\n  ")
+          print("")  # <- Aesthetic choice
+
+          if response.strip().lower() == "y":
+            self.active_block = CodeBlock()
+            self.active_block.language = language
+            self.active_block.code = code
+          else:
             self.active_block.end()
-            language = self.active_block.language
-            code = self.active_block.code
+            self.messages.append({
+              "role":
+              "function",
+              "name":
+              "run_code",
+              "content":
+              "User decided not to run this code."
+            })
+            return
 
-            response = input("  Would you like to run this code? (y/n)\n\n  ")
-            print("")  # <- Aesthetic choice
-
-            if response.strip().lower() == "y":
-              # Create a new, identical block where the code will actually be run
-              self.active_block = CodeBlock()
-              self.active_block.language = language
-              self.active_block.code = code
-            else:
-              # User declined to run code.
-              self.active_block.end()
-              self.messages.append({
-                "role":
-                "function",
-                "name":
-                "run_code",
-                "content":
-                "User decided not to run this code."
-              })
-              return
-
-          # If we couldn't parse its arguments, we need to try again.
           if not self.local and "parsed_arguments" not in self.messages[-1]["function_call"]:
-
-            # After collecting some data via the below instruction to users,
-            # This is the most common failure pattern: https://github.com/KillianLucas/open-interpreter/issues/41
-
-            # print("> Function call could not be parsed.\n\nPlease open an issue on Github (openinterpreter.com, click Github) and paste the following:")
-            # print("\n", self.messages[-1]["function_call"], "\n")
-            # time.sleep(2)
-            # print("Informing the language model and continuing...")
-
-            # Since it can't really be fixed without something complex,
-            # let's just berate the LLM then go around again.
-
             self.messages.append({
               "role": "function",
               "name": "run_code",
               "content": """Your function call could not be parsed. Please use ONLY the `run_code` function, which takes two parameters: `code` and `language`. Your response should be formatted as a JSON."""
             })
-
             self.respond()
             return
 
-          # Create or retrieve a Code Interpreter for this language
           language = self.messages[-1]["function_call"]["parsed_arguments"]["language"]
           if language not in self.code_interpreters:
             self.code_interpreters[language] = CodeInterpreter(language, self.debug_mode)
           code_interpreter = self.code_interpreters[language]
 
-          # Let this Code Interpreter control the active_block
           code_interpreter.active_block = self.active_block
           code_interpreter.run()
 
-          # End the active_block
           self.active_block.end()
 
-          # Append the output to messages
-          # Explicitly tell it if there was no output (sometimes "" = hallucinates output)
           self.messages.append({
             "role": "function",
             "name": "run_code",
